52dde28ff16b9137af9d070153bb48c91ed32457
Yashua Pervez <yashuapervez@gmail.com>
2021-11-12 00:47:52 +0500

    textarea support added to input component

--- components/FormElements/Input.tsx
+++ components/FormElements/Input.tsx
@@ -7,6 +7,8 @@ type InputProps = {
   error?: string;
   inline?: boolean;
   type?: "text" | "password";
+  textarea?: boolean;
+  initialValue?: string;
 };
 
 const Input: React.FC<InputProps> = ({
@@ -14,16 +16,41 @@ const Input: React.FC<InputProps> = ({
   placeholder,
   error,
   inline = false,
+  textarea = false,
+  initialValue,
   ...rest
 }) => {
   const { register } = useFormContext();
+  const commonClasses =
+    "bg-white border border-gray-200 w-full outline-none py-2 px-3 rounded focus:border-blue-400";
+
+  if (textarea) {
+    return (
+      <div>
+        <textarea
+          id={id}
+          placeholder={placeholder}
+          className={`${commonClasses} resize-none h-36`}
+          defaultValue={initialValue}
+          {...rest}
+          {...register(id)}
+        ></textarea>
+        {error && (
+          <span className="text-sm ml-1 block text-red-400 absolute b-0 -mt-1">
+            {error}
+          </span>
+        )}
+      </div>
+    );
+  }
 
   return (
     <div className={`${inline ? "mr-3" : "mb-6"} relative w-full`}>
       <input
         id={id}
         placeholder={placeholder}
-        className={`bg-white border border-gray-200 w-full outline-none py-2 px-3 rounded focus:border-blue-400 h-12`}
+        className={`${commonClasses} h-12`}
+        defaultValue={initialValue}
         {...rest}
         {...register(id)}
       />
