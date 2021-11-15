805c7d55ff5f787454b95ae99173c65f25df53b6
Yashua Pervez <yashuapervez@gmail.com>
2021-11-16 00:37:59 +0500

    avatar component extended

--- components/UI/Avatar.tsx
+++ components/UI/Avatar.tsx
@@ -1,11 +1,32 @@
 type AvatarProps = {
   src: string;
+  size?: "sm" | "md" | "lg";
+  moreJSX?: React.ReactNode;
 };
 
-const Avatar: React.FC<AvatarProps> = ({ src }) => {
+const Avatar: React.FC<AvatarProps> = ({ src, size = "sm", moreJSX }) => {
+  let sizeClasses = "";
+  switch (size) {
+    case "sm":
+      sizeClasses = "h-10 w-10";
+      break;
+    case "md":
+      sizeClasses = "h-20 w-20";
+      break;
+    case "lg":
+      sizeClasses = "h-36 w-36";
+      break;
+    default:
+      sizeClasses = "h-10 w-10";
+      break;
+  }
+
   return (
-    <div className="h-10 w-10 bg-gray-300 rounded-full overflow-hidden">
+    <div
+      className={`${sizeClasses} bg-gray-300 rounded-full overflow-hidden relative`}
+    >
       <img src={src} className="min-w-full min-h-full object-cover" />
+      {moreJSX}
     </div>
   );
 };
