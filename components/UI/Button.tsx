9d9d5a90cbd6146ed48669b5f7e7355f7163b3d5
Yashua Pervez <yashuapervez@gmail.com>
2021-11-16 00:37:39 +0500

    button onclick support added

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -5,6 +5,7 @@ type ButtonProps = {
   type?: "button" | "submit" | "reset";
   className?: string;
   loading?: boolean;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const Button: React.FC<ButtonProps> = ({
@@ -12,12 +13,14 @@ const Button: React.FC<ButtonProps> = ({
   type = "button",
   className,
   loading = false,
+  onClick,
 }) => {
   return (
     <button
       type={type}
       className={`px-5 py-1 bg-blue-400 border border-blue-500 hover:bg-blue-500 hover:border-blue-600 rounded ${className}`}
       disabled={loading}
+      onClick={onClick}
     >
       <span className="text-white text-sm font-semibold">{children}</span>
     </button>
