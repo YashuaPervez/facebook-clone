35354a81f39c0f45c87d9f7373adecb2f96eea49
Yashua Pervez <yashuapervez@gmail.com>
2021-11-24 02:16:18 +0500

    type defs added

---
+++ typeDefs.ts
@@ -0,0 +1,45 @@
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  wallPosts: Post[];
+  profile: Profile;
+}
+
+export interface Profile {
+  displayName: string;
+  about?: string;
+  imageURL?: string;
+  interests?: string;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  imageURL?: string;
+  createdAt: string;
+  updatedAt: string;
+  liked: boolean;
+  author: User;
+  comments: Comment[];
+  likes: Like[];
+}
+
+export interface Comment {
+  id: number;
+  text: string;
+  createdAt: string;
+  updatedAt: string;
+  author: User;
+  post: Post;
+}
+
+export interface Like {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  liker: User;
+  post: Post;
+}
