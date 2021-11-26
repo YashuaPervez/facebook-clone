fef432bd57b277f9ebb02940d8846ad7c202949d
Yashua Pervez <yashuapervez@gmail.com>
2021-11-27 03:32:12 +0500

    auth queries made reuseable

--- utils/queries/authQueries.ts
+++ utils/queries/authQueries.ts
@@ -1,5 +1,62 @@
 import { gql } from "@apollo/client";
 
+const userFullProfile = `
+id
+username
+email
+profile {
+  displayName
+  imageURL
+  about
+  interests
+  coverImageURL
+  workPlace
+  location
+}
+wallPosts {
+  id
+  title
+  imageURL
+  createdAt
+  liked
+  author {
+    id
+    username
+    profile {
+      displayName
+      imageURL
+    }
+  }
+  comments {
+    moreAvailable
+    comments {
+      id
+      text
+      createdAt
+      author {
+        id
+        username
+        profile {
+          displayName
+          imageURL
+        }
+      }
+    }
+  }
+  likes {
+    createdAt
+    liker {
+      id
+      username
+      profile {
+        displayName
+        imageURL
+      }
+    }
+  }
+}
+`;
+
 export const signupMutation = gql`
   mutation Signup($email: String!, $password: String!, $username: String!) {
     signup(data: { email: $email, password: $password, username: $username }) {
@@ -12,6 +69,9 @@ export const loginMutation = gql`
   mutation Login($email: String!, $password: String!) {
     login(data: { email: $email, password: $password }) {
       token
+      user {
+        ${userFullProfile}
+      }
     }
   }
 `;
@@ -19,60 +79,7 @@ export const loginMutation = gql`
 export const getUserProfileQuery = gql`
   query {
     me {
-      id
-      username
-      email
-      profile {
-        displayName
-        imageURL
-        about
-        interests
-        coverImageURL
-        workPlace
-        location
-      }
-      wallPosts {
-        id
-        title
-        imageURL
-        createdAt
-        liked
-        author {
-          id
-          username
-          profile {
-            displayName
-            imageURL
-          }
-        }
-        comments {
-          moreAvailable
-          comments {
-            id
-            text
-            createdAt
-            author {
-              id
-              username
-              profile {
-                displayName
-                imageURL
-              }
-            }
-          }
-        }
-        likes {
-          createdAt
-          liker {
-            id
-            username
-            profile {
-              displayName
-              imageURL
-            }
-          }
-        }
-      }
+      ${userFullProfile}
     }
   }
 `;
