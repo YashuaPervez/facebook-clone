cba482834e2741fb1e341a9fb252dcf3ec7d8aee
Yashua Pervez <yashuapervez@gmail.com>
2021-11-28 01:33:28 +0500

    search page mobile view fixed

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useRouter } from "next/router";
 
 // Components
+import BodyLayout from "../components/BodyLayout";
 import UserSearch from "../components/User/UserSearch";
 import UserCardList from "../components/User/UserCardList";
 
@@ -10,18 +11,13 @@ const Search = () => {
   const { q: urlQuery } = query;
 
   return (
-    <div className="flex gap-4">
-      <div className="w-72"></div>
-      <div className="flex-1">
-        <div className="mb-2">
-          <UserSearch block />
-        </div>
-        {urlQuery && (
-          <div className="mb-6">Showing results for "{urlQuery}"</div>
-        )}
-        <UserCardList query={urlQuery as string} />
+    <BodyLayout leftSide={<div></div>} rightSide={null} removePannelButtons>
+      <div className="mb-2">
+        <UserSearch block />
       </div>
-    </div>
+      {urlQuery && <div className="mb-6">Showing results for "{urlQuery}"</div>}
+      <UserCardList query={urlQuery as string} />
+    </BodyLayout>
   );
 };
 
