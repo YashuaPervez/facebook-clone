b7b9062e550c9c9812fa7907a25a3627c8794f37
Yashua Pervez <yashuapervez@gmail.com>
2021-11-28 13:19:54 +0500

    tailwind colors added

---
+++ styles/colors.js
@@ -0,0 +1,8 @@
+const colors = {
+  primary: {
+    main: "#2C62E0",
+    dark: "#0f307d",
+  },
+};
+
+module.exports = { colors };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+const { colors } = require("./styles/colors");
+
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: { ...colors },
+    },
   },
   variants: {
     extend: {
